e307b85adf8a2c0a527231b1e06744819167d706
expert <jacksweat93@outlook.com>
2020-03-25 10:18:40 -0500

    vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
+import {sync} from 'vuex-router-sync';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false;
 
+Vue.config.productionTip = false
+Vue.use(Vuetify);
+
+sync(store, router);
 new Vue({
   router,
   store,
-  render(h) { return h(App); },
-}).$mount('#app');
+  vuetify,
+  render: function (h) { return h(App) }
+}).$mount('#app')
